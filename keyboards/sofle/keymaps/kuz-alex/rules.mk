753a7e086c105ac4bdd601a68017a41f1a782494
Alexander Kuznetsov <d1rd3v@gmail.com>
2023-12-24 15:27:02 +0500

    Rgb matrix configuration

--- keyboards/sofle/keymaps/kuz-alex/rules.mk
+++ keyboards/sofle/keymaps/kuz-alex/rules.mk
@@ -3,8 +3,11 @@ OLED_ENABLE = yes
 CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes
 LTO_ENABLE = yes
-RGBLIGHT_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
 
 # Enabling link-time optimization saves about 3 kilobytes.
 # https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
